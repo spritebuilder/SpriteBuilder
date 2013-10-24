4e62c725fac87f9daeb786c3ec154ed9a3028793
Viktor <viktorlidholt@gmail.com>
2013-10-24 09:44:47 -0700

    Adds support for CCTextField

---
+++ SpriteBuilder/CCTextField/CCBPTextField.h
@@ -0,0 +1,13 @@
+//
+//  CCBPTextField.h
+//  SpriteBuilder
+//
+//  Created by Viktor on 10/24/13.
+//
+//
+
+#import "CCTextField.h"
+
+@interface CCBPTextField : CCTextField
+
+@end
---
+++ SpriteBuilder/CCTextField/CCBPTextField.m
@@ -0,0 +1,23 @@
+//
+//  CCBPTextField.m
+//  SpriteBuilder
+//
+//  Created by Viktor on 10/24/13.
+//
+//
+
+#import "CCBPTextField.h"
+
+@implementation CCBPTextField
+
+- (id) init
+{
+    self = [super init];
+    if (!self) return NULL;
+    
+    self.userInteractionEnabled = NO;
+    
+    return self;
+}
+
+@end
