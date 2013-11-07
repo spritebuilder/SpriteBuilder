37ee687c1ca15e766a4de8766bf40b3eac9831b2
jtwigg <john.twigg@gmail.com>
2013-11-07 14:15:25 -0800

    @Issue#17 initial working hack display of audio waveform

--- SpriteBuilder/ccBuilder/ResourceManagerPreviewView.m
+++ SpriteBuilder/ccBuilder/ResourceManagerPreviewView.m
@@ -29,6 +29,7 @@
 #import "AppDelegate.h"
 #import "ProjectSettings.h"
 #import "FCFormatConverter.h"
+#import <AVFoundation/AVFoundation.h>
 
 @implementation ResourceManagerPreviewView
 
@@ -173,7 +174,7 @@
             NSImage* icon = [[NSWorkspace sharedWorkspace] iconForFileType:@"wav"];
             [icon setScalesWhenResized:YES];
             icon.size = NSMakeSize(128, 128);
-            [previewSoundImage setImage:icon];
+            //[previewSoundImage setImage:icon];
             
             // Update sound
             QTMovie* movie = [QTMovie movieWithFile:res.filePath error:NULL];
@@ -186,6 +187,12 @@
             self.enabled = YES;
             
             [viewSound setHidden:NO];
+            
+            
+            NSImage * waveformImage = [self renderImageForAudioAsset:[AVURLAsset assetWithURL:[NSURL fileURLWithPath:res.filePath]]];
+            
+            [previewSoundImage setImage:waveformImage];
+            
         }
         else if (res.type == kCCBResTypeCCBFile)
         {
@@ -564,6 +571,248 @@
     {
         [settings setValue:[NSNumber numberWithInt:format_android_sound_quality] forResource:_previewedResource andKey:@"format_android_sound_quality"];
     }
+}
+
+#pragma mark Audio Display
+
+- (NSImage *) renderImageForAudioAsset:(AVURLAsset *)audioAssets {
+    
+    NSError * error = nil;
+    AVAssetReader * reader = [[AVAssetReader alloc] initWithAsset:audioAssets error:&error];
+    AVAssetTrack * songTrack = [audioAssets.tracks objectAtIndex:0];
+    
+    NSDictionary* outputSettingsDict = [[NSDictionary alloc] initWithObjectsAndKeys:
+                                        
+                                        [NSNumber numberWithInt:kAudioFormatLinearPCM],AVFormatIDKey,
+                                        //     [NSNumber numberWithInt:44100.0],AVSampleRateKey, /*Not Supported*/
+                                        //     [NSNumber numberWithInt: 2],AVNumberOfChannelsKey,    /*Not Supported*/
+                                        
+                                        [NSNumber numberWithInt:16],AVLinearPCMBitDepthKey,
+                                        [NSNumber numberWithBool:NO],AVLinearPCMIsBigEndianKey,
+                                        [NSNumber numberWithBool:NO],AVLinearPCMIsFloatKey,
+                                        [NSNumber numberWithBool:NO],AVLinearPCMIsNonInterleaved,
+                                        
+                                        nil];
+    
+    
+    AVAssetReaderTrackOutput* output = [[AVAssetReaderTrackOutput alloc] initWithTrack:songTrack outputSettings:outputSettingsDict];
+    
+    [reader addOutput:output];
+    [output release];
+    
+    UInt32 sampleRate,channelCount;
+    
+    NSArray* formatDesc = songTrack.formatDescriptions;
+    for(unsigned int i = 0; i < [formatDesc count]; ++i) {
+        CMAudioFormatDescriptionRef item = (CMAudioFormatDescriptionRef)[formatDesc objectAtIndex:i];
+        const AudioStreamBasicDescription* fmtDesc = CMAudioFormatDescriptionGetStreamBasicDescription (item);
+        if(fmtDesc ) {
+            
+            sampleRate = fmtDesc->mSampleRate;
+            channelCount = fmtDesc->mChannelsPerFrame;
+            
+            //    NSLog(@"channels:%u, bytes/packet: %u, sampleRate %f",fmtDesc->mChannelsPerFrame, fmtDesc->mBytesPerPacket,fmtDesc->mSampleRate);
+        }
+    }
+    
+    
+    UInt32 bytesPerSample = 2 * channelCount;
+    SInt16 normalizeMax = 0;
+    
+    NSMutableData * fullSongData = [[NSMutableData alloc] init];
+    [reader startReading];
+    
+    
+    UInt64 totalBytes = 0;
+    SInt64 totalLeft = 0;
+    SInt64 totalRight = 0;
+    NSInteger sampleTally = 0;
+    
+    NSInteger samplesPerPixel = 0;//sampleRate / 50;
+    
+    while (reader.status == AVAssetReaderStatusReading){
+        
+        AVAssetReaderTrackOutput * trackOutput = (AVAssetReaderTrackOutput *)[reader.outputs objectAtIndex:0];
+        CMSampleBufferRef sampleBufferRef = [trackOutput copyNextSampleBuffer];
+        
+        if (sampleBufferRef){
+            CMBlockBufferRef blockBufferRef = CMSampleBufferGetDataBuffer(sampleBufferRef);
+            
+            size_t length = CMBlockBufferGetDataLength(blockBufferRef);
+            totalBytes += length;
+            
+            
+            NSAutoreleasePool *wader = [[NSAutoreleasePool alloc] init];
+            
+            NSMutableData * data = [NSMutableData dataWithLength:length];
+            CMBlockBufferCopyDataBytes(blockBufferRef, 0, length, data.mutableBytes);
+            
+            
+            SInt16 * samples = (SInt16 *) data.mutableBytes;
+            int sampleCount = length / bytesPerSample;
+            for (int i = 0; i < sampleCount ; i ++) {
+                
+                SInt16 left = *samples++;
+                
+                totalLeft  += left;
+                
+                
+                
+                SInt16 right;
+                if (channelCount==2) {
+                    right = *samples++;
+                    
+                    totalRight += right;
+                }
+                
+                sampleTally++;
+                
+                if (sampleTally > samplesPerPixel) {
+                    
+                    left  = totalLeft / sampleTally;
+                    
+                    SInt16 fix = abs(left);
+                    if (fix > normalizeMax) {
+                        normalizeMax = fix;
+                    }
+                    
+                    
+                    [fullSongData appendBytes:&left length:sizeof(left)];
+                    
+                    if (channelCount==2) {
+                        right = totalRight / sampleTally;
+                        
+                        
+                        SInt16 fix = abs(right);
+                        if (fix > normalizeMax) {
+                            normalizeMax = fix;
+                        }
+                        
+                        
+                        [fullSongData appendBytes:&right length:sizeof(right)];
+                    }
+                    
+                    totalLeft   = 0;
+                    totalRight  = 0;
+                    sampleTally = 0;
+                    
+                }
+            }
+            
+            
+            
+            [wader drain];
+            
+            
+            CMSampleBufferInvalidate(sampleBufferRef);
+            
+            CFRelease(sampleBufferRef);
+        }
+    }
+    
+    
+    NSImage *waveformImage = nil;
+    
+    if (reader.status == AVAssetReaderStatusFailed || reader.status == AVAssetReaderStatusUnknown){
+        // Something went wrong. return nil
+        
+        return nil;
+    }
+    
+    if (reader.status == AVAssetReaderStatusCompleted){
+        
+        NSLog(@"rendering output graphics using normalizeMax %d",normalizeMax);
+        
+        waveformImage = [self audioImageGraph:(SInt16 *)
+                         fullSongData.bytes 
+                                 normalizeMax:normalizeMax 
+                                  sampleCount:fullSongData.length / 4 
+                                 channelCount:channelCount
+                                  imageHeight:previewSoundImage.frame.size.height];
+        
+ 
+    }
+    
+    [fullSongData release];
+    [reader release];
+    
+    return waveformImage;
+}
+
+
+-(NSImage *) audioImageGraph:(SInt16 *) samples
+                normalizeMax:(SInt16) normalizeMax
+                 sampleCount:(NSInteger) sampleCount
+                channelCount:(NSInteger) channelCount
+                 imageHeight:(float) imageHeight {
+    
+    CGSize imageSize = CGSizeMake(sampleCount, imageHeight);
+ 
+
+    NSBitmapImageRep *offscreenRep = [[[NSBitmapImageRep alloc]
+                                       initWithBitmapDataPlanes:NULL
+                                       pixelsWide:sampleCount
+                                       pixelsHigh:imageHeight
+                                       bitsPerSample:8
+                                       samplesPerPixel:4
+                                       hasAlpha:YES
+                                       isPlanar:NO
+                                       colorSpaceName:NSDeviceRGBColorSpace
+                                       bitmapFormat:NSAlphaFirstBitmapFormat
+                                       bytesPerRow:0
+                                       bitsPerPixel:0] autorelease];
+    
+    NSGraphicsContext * graphicsContext = [NSGraphicsContext graphicsContextWithBitmapImageRep:offscreenRep];
+    
+    CGContextRef context = [graphicsContext graphicsPort];
+    
+    CGContextSetFillColorWithColor(context, [NSColor blackColor].CGColor);
+    CGContextSetAlpha(context,1.0);
+    CGRect rect;
+    rect.size = imageSize;
+    rect.origin.x = 0;
+    rect.origin.y = 0;
+    
+    CGColorRef leftcolor = [[NSColor whiteColor] CGColor];
+    CGColorRef rightcolor = [[NSColor redColor] CGColor];
+    
+    CGContextFillRect(context, rect);
+    
+    CGContextSetLineWidth(context, 1.0);
+    
+    float halfGraphHeight = (imageHeight / 2) / (float) channelCount ;
+    float centerLeft = halfGraphHeight;
+    float centerRight = (halfGraphHeight*3) ;
+    float sampleAdjustmentFactor = (imageHeight/ (float) channelCount) / (float) normalizeMax / 2.0f;
+    
+    for (NSInteger intSample = 0 ; intSample < sampleCount ; intSample ++ ) {
+        SInt16 left = *samples++;
+        float pixels = (float) left;
+        pixels *= sampleAdjustmentFactor;
+        CGContextMoveToPoint(context, intSample, centerLeft-pixels);
+        CGContextAddLineToPoint(context, intSample, centerLeft+pixels);
+        CGContextSetStrokeColorWithColor(context, leftcolor);
+        CGContextStrokePath(context);
+        
+        if (channelCount==2) {
+            SInt16 right = *samples++;
+            float pixels = (float) right;
+            pixels *= sampleAdjustmentFactor;
+            CGContextMoveToPoint(context, intSample, centerRight - pixels);
+            CGContextAddLineToPoint(context, intSample, centerRight + pixels);
+            CGContextSetStrokeColorWithColor(context, rightcolor);
+            CGContextStrokePath(context);
+        }
+    }
+    
+    [NSGraphicsContext restoreGraphicsState];
+    
+    NSImage *img = [[[NSImage alloc] initWithSize:imageSize] autorelease];
+    [img addRepresentation:offscreenRep];
+
+    
+    
+    return img;
 }
 
 #pragma mark Split view constraints
