c305dfb84c8570d42cc6ac5ed760e1b6897fec80
jtwigg <john.twigg@gmail.com>
2013-11-12 13:56:15 -0800

    @Issue#33 Add hotkey for Skew Keyframe

--- SpriteBuilder/ccBuilder/AppDelegate.m
+++ SpriteBuilder/ccBuilder/AppDelegate.m
@@ -4173,6 +4173,7 @@ static BOOL hideAllToNextSeparator;
     else if (tag == 4) return @"displayFrame";
     else if (tag == 5) return @"opacity";
     else if (tag == 6) return @"color";
+    else if (tag == 7) return @"skew";
     else return NULL;
 }
 
