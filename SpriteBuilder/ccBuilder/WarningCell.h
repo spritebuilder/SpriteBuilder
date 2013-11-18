e0a702434f4c35374db2eafdc61fc8609d09ac3d
jtwigg <john.twigg@gmail.com>
2013-11-18 11:16:31 -0800

    @Issue#44 add warning icons to Warning Cells

--- SpriteBuilder/ccBuilder/WarningCell.h
+++ SpriteBuilder/ccBuilder/WarningCell.h
@@ -8,6 +8,6 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface WarningCellController : NSViewController
+@interface WarningCell : NSCell
 
 @end
--- SpriteBuilder/ccBuilder/WarningCell.m
+++ SpriteBuilder/ccBuilder/WarningCell.m
@@ -6,21 +6,27 @@
 //
 //
 
-#import "WarningCellController.h"
+#import "WarningCell.h"
+@implementation WarningCell
 
-@interface WarningCellController ()
-
-@end
-
-@implementation WarningCellController
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+- (void) drawWithFrame:(NSRect)cellFrame inView:(NSView *)controlView
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Initialization code here.
-    }
-    return self;
+    
+    NSGraphicsContext* gc = [NSGraphicsContext currentContext];
+    [gc saveGraphicsState];
+
+    NSImage * warningImage = [NSImage imageNamed:@"inspector-warning.png"];
+    
+    [warningImage drawInRect:CGRectMake(cellFrame.origin.x + 1,cellFrame.origin.y+1, 13, 13) fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1 respectFlipped:YES hints:nil];
+
+    NSString * warningText = self.objectValue;
+    [warningText drawAtPoint:NSMakePoint(cellFrame.origin.x + warningImage.size.width +  3,cellFrame.origin.y+1) withAttributes:nil];
+    
+    
+    [gc restoreGraphicsState];
+    
 }
+
+
 
 @end
