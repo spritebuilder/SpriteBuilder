c5052d6e72301e485b584de2464de11bfda52f5b
Viktor <viktorlidholt@gmail.com>
2013-12-18 18:13:36 -0800

    Excludes unfinished docs

--- SpriteBuilder/ccBuilder/HelpWindow.m
+++ SpriteBuilder/ccBuilder/HelpWindow.m
@@ -45,6 +45,9 @@
     mdFiles = [[NSMutableArray alloc] init];
     for (NSString* file in files)
     {
+#warning This part of documentation should be updated too
+        if ([file characterAtIndex:0] == 'X') continue;
+        
         if ([file hasSuffix:@".md"])
         {
             HelpPage* hp = [[[HelpPage alloc] init] autorelease];
