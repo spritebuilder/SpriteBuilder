1f54d0afe19d3f668de981f1b8179ea195dfdf7a
jtwigg <john.twigg@gmail.com>
2013-12-23 16:46:15 -0800

    remove png reader.

--- SpriteBuilder/libs/FCFormatConverter/FCFormatConverter.mm
+++ SpriteBuilder/libs/FCFormatConverter/FCFormatConverter.mm
@@ -9,7 +9,6 @@
 #import "FCFormatConverter.h"
 #import "PVRTexture.h"
 #import "PVRTextureUtilities.h"
-#import "PNGReader.h"
 
 static FCFormatConverter* gDefaultConverter = NULL;
 
@@ -115,15 +114,6 @@ static FCFormatConverter* gDefaultConverter = NULL;
         // PVR(TC) image
         NSString *dstPath = [[srcPath stringByDeletingPathExtension] stringByAppendingPathExtension:@"pvr"];
         
-        NSString* formatStr = NULL;
-        if (format == kFCImageFormatPVR_RGBA8888)      formatStr = @"r8g8b8a8,UBN,lRGB";
-        else if (format == kFCImageFormatPVR_RGBA4444) formatStr = @"r4g4b4a4,USN,lRGB";
-        else if (format == kFCImageFormatPVR_RGB565)   formatStr = @"r5g6b5,USN,lRGB";
-        else if (format == kFCImageFormatPVRTC_4BPP)   formatStr = @"PVRTC1_4,UBN,lRGB";
-        else if (format == kFCImageFormatPVRTC_2BPP)   formatStr = @"PVRTC1_2,UBN,lRGB";
-        
-        
-        
         pvrtexture::PixelType pixelType;
         EPVRTVariableType variableType = ePVRTVarTypeUnsignedByteNorm;
         
@@ -150,46 +140,23 @@ static FCFormatConverter* gDefaultConverter = NULL;
             pixelType = pvrtexture::PixelType(ePVRTPF_PVRTCI_2bpp_RGB);
         }
 
-
+        NSImage * image = [[NSImage alloc] initWithContentsOfFile:srcPath];
+        NSBitmapImageRep* rawImg = [NSBitmapImageRep imageRepWithData:[image TIFFRepresentation]];
         
-        
-        // Convert PNG to PVR(TC)
-        
-        /*
-        NSTask* pvrTask = [[NSTask alloc] init];
-        [pvrTask setCurrentDirectoryPath:dstDir];
-        [pvrTask setLaunchPath:[[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"PVRTexToolCL"]];
-        NSMutableArray* args = [NSMutableArray arrayWithObjects:
-                                @"-i", srcPath,
-                                @"-o", dstPath,
-                                @"-p",
-                                @"-legacypvr",
-                                @"-f", formatStr,
-                                @"-q", @"pvrtcbest",
-                                nil];
-        if (dither) [args addObject:@"-dither"];
-        [pvrTask setArguments:args];
-        [pvrTask launch];
-        [pvrTask waitUntilExit];
-        [pvrTask release];
-        */
-        
-        
-        PNGReader reader([srcPath UTF8String]);
-        
-        pvrtexture::CPVRTextureHeader header(pvrtexture::PVRStandard8PixelType.PixelTypeID, reader.width() , reader.height());
-        pvrtexture::CPVRTexture * pvrTexture = new pvrtexture::CPVRTexture(header , reader.data());
-        //pvrTexture->setIsFileCompressed(YES);
+        pvrtexture::CPVRTextureHeader header(pvrtexture::PVRStandard8PixelType.PixelTypeID, image.size.width , image.size.height);
+        pvrtexture::CPVRTexture     * pvrTexture = new pvrtexture::CPVRTexture(header , rawImg.bitmapData);
         
         if(!Transcode(*pvrTexture, pixelType, variableType, ePVRTCSpacelRGB, pvrtexture::ePVRTCBest, dither))
         {
-            int break_here = 1;
+            #warning TODO: How do I bubble up errors from here.
+            int break_here =1;
         }
         
         CPVRTString filePath([dstPath UTF8String], dstPath.length);
         
         if(!pvrTexture->saveFileLegacyPVR(filePath,  pvrtexture::eOGLES2))
         {
+            #warning TODO: How do I bubble up errors from here.
             int break_here =1;
         }
         
