b7afcd6e63adcddce3dd94387a8c1801e3321109
Steffen Itterheim <steffen@learn-cocos2d.com>
2014-01-17 20:10:00 +0100

    fix wording on color node descriptions

---
+++ Test/SpriteKitReader-Experimental/SpriteKitReader-Experimental/CCBReader/SpriteKit/SKLabelNode+CCBReader.h
@@ -0,0 +1,13 @@
+//
+//  SKLabelNode+CCBReader.h
+//  SpriteKitReader-Experimental
+//
+//  Created by Steffen Itterheim on 17/01/14.
+//  Copyright (c) 2014 Steffen Itterheim. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface SKLabelNode (CCBReader)
+
+@end
---
+++ Test/SpriteKitReader-Experimental/SpriteKitReader-Experimental/CCBReader/SpriteKit/SKLabelNode+CCBReader.m
@@ -0,0 +1,13 @@
+//
+//  SKLabelNode+CCBReader.m
+//  SpriteKitReader-Experimental
+//
+//  Created by Steffen Itterheim on 17/01/14.
+//  Copyright (c) 2014 Steffen Itterheim. All rights reserved.
+//
+
+#import "SKLabelNode+CCBReader.h"
+
+@implementation SKLabelNode (CCBReader)
+
+@end
