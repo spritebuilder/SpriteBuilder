da9521c7015a211a39f0c2a09fc633e0a910fa33
Steffen Itterheim <steffen@learn-cocos2d.com>
2014-01-26 00:28:17 +0100

    implemented node filtering based on project's "engine" type

--- SpriteBuilder/ccBuilder/PlugInNode.m
+++ SpriteBuilder/ccBuilder/PlugInNode.m
@@ -117,6 +117,12 @@
     NSURL* propsURL = [bundle URLForResource:@"CCBPProperties" withExtension:@"plist"];
     NSMutableDictionary* props = [NSMutableDictionary dictionaryWithContentsOfURL:propsURL];
     
+	_targetEngine = CCBTargetEngineCocos2d;
+	if ([[[props objectForKey:@"targetEngine"] lowercaseString] isEqualToString:@"spritekit"])
+	{
+		_targetEngine = CCBTargetEngineSpriteKit;
+	}
+	
     nodeClassName = [props objectForKey:@"className"];
     nodeEditorClassName = [props objectForKey:@"editorClassName"];
     
