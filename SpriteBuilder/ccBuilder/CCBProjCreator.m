12cc9a0375fe2b2e7b0fdbe3a6f0bf92eeeba282
Steffen Itterheim <steffen@learn-cocos2d.com>
2014-01-25 23:24:48 +0100

    added menu item to create sprite kit project added "engine" setting to ProjectSettings to differentiate CC vs SK updated ProjCreator to create different project based on "engine"

--- SpriteBuilder/ccBuilder/CCBProjCreator.m
+++ SpriteBuilder/ccBuilder/CCBProjCreator.m
@@ -11,16 +11,23 @@
 
 @implementation CCBProjCreator
 
-- (BOOL) createDefaultProjectAtPath:(NSString*)fileName
+-(BOOL) createDefaultProjectAtPath:(NSString*)fileName engine:(CCBTargetEngine)engine
 {
     NSFileManager* fm = [NSFileManager defaultManager];
     
-    NSString* zipFile = [[NSBundle mainBundle] pathForResource:@"PROJECTNAME" ofType:@"zip" inDirectory:@"Generated"];
+	NSString* substitutableProjectName = @"PROJECTNAME";
+	if (engine == CCBTargetEngineSpriteKit)
+	{
+		substitutableProjectName = [NSString stringWithFormat:@"SPRITEKIT%@", substitutableProjectName];
+	}
+	
+    NSString* zipFile = [[NSBundle mainBundle] pathForResource:substitutableProjectName ofType:@"zip" inDirectory:@"Generated"];
     
     // Check that zip file exists
     if (![fm fileExistsAtPath:zipFile])
     {
-        [[AppDelegate appDelegate] modalDialogTitle:@"Failed to Create Project" message:@"The default SpriteBuilder project is missing from this build. Make sure that you build SpriteBuilder using 'Scripts/BuildDistribution.sh <versionstr>' the first time you build the program."];
+        [[AppDelegate appDelegate] modalDialogTitle:@"Failed to Create Project"
+											message:@"The default SpriteBuilder project is missing from this build. Make sure that you build SpriteBuilder using 'Scripts/BuildDistribution.sh <versionstr>' the first time you build the program."];
         return NO;
     }
     
@@ -34,23 +41,26 @@
     [zipTask waitUntilExit];
     
     // Rename ccbproj
-    [fm moveItemAtPath:[[fileName stringByDeletingLastPathComponent] stringByAppendingPathComponent:@"PROJECTNAME.ccbproj"] toPath:fileName error:NULL];
+	NSString* ccbproj = [NSString stringWithFormat:@"%@.ccbproj", substitutableProjectName];
+    [fm moveItemAtPath:[[fileName stringByDeletingLastPathComponent] stringByAppendingPathComponent:ccbproj] toPath:fileName error:NULL];
     
     // Update the Xcode project
-    NSString* xcodeFileName = [[fileName stringByDeletingLastPathComponent] stringByAppendingPathComponent:@"PROJECTNAME.xcodeproj"];
+	NSString* xcodeproj = [NSString stringWithFormat:@"%@.xcodeproj", substitutableProjectName];
+    NSString* xcodeFileName = [[fileName stringByDeletingLastPathComponent] stringByAppendingPathComponent:xcodeproj];
     NSString* projName = [[fileName lastPathComponent] stringByDeletingPathExtension];
     
     // Update the project
-    [self setName:projName inFile:[xcodeFileName stringByAppendingPathComponent:@"project.pbxproj"]];
+    [self setName:projName inFile:[xcodeFileName stringByAppendingPathComponent:@"project.pbxproj"] projectName:substitutableProjectName];
     
     // Update workspace data
-    [self setName:projName inFile:[xcodeFileName stringByAppendingPathComponent:@"project.xcworkspace/contents.xcworkspacedata"]];
+    [self setName:projName inFile:[xcodeFileName stringByAppendingPathComponent:@"project.xcworkspace/contents.xcworkspacedata"] projectName:substitutableProjectName];
     
     // Update scheme
-    [self setName:projName inFile:[xcodeFileName stringByAppendingPathComponent:@"xcshareddata/xcschemes/PROJECTNAME.xcscheme"]];
+	NSString* xcscheme = [NSString stringWithFormat:@"xcshareddata/xcschemes/%@.xcscheme", substitutableProjectName];
+    [self setName:projName inFile:[xcodeFileName stringByAppendingPathComponent:xcscheme] projectName:substitutableProjectName];
     
     // Rename scheme file
-    NSString* schemeFile = [xcodeFileName stringByAppendingPathComponent:@"xcshareddata/xcschemes/PROJECTNAME.xcscheme"];
+    NSString* schemeFile = [xcodeFileName stringByAppendingPathComponent:xcscheme];
     NSString* newSchemeFile = [[[schemeFile stringByDeletingLastPathComponent] stringByAppendingPathComponent:projName] stringByAppendingPathExtension:@"xcscheme"];
     [fm moveItemAtPath:schemeFile toPath:newSchemeFile error:NULL];
     
@@ -62,18 +72,27 @@
     return [fm fileExistsAtPath:fileName];
 }
 
-- (void) setName:(NSString*) name inFile:(NSString*)fileName
+- (void) setName:(NSString*)name inFile:(NSString*)fileName projectName:(NSString*)projectName
 {
-    NSString* regExp = [NSString stringWithFormat:@"s/PROJECTNAME/%@/g", name];
+    NSString* regExp = [NSString stringWithFormat:@"s/%@/%@/g", projectName, name];
     
-    NSTask* renameTask = [[NSTask alloc] init];
-    [renameTask setCurrentDirectoryPath:[fileName stringByDeletingLastPathComponent]];
-    [renameTask setLaunchPath:@"/usr/bin/sed"];
-    NSArray* args = [NSArray arrayWithObjects:@"-ie", regExp, fileName, nil];
-    NSLog(@"ARGS: %@", args);
-    [renameTask setArguments:args];
-    [renameTask launch];
-    [renameTask waitUntilExit];
+	@try {
+		NSTask* renameTask = [[NSTask alloc] init];
+		[renameTask setCurrentDirectoryPath:[fileName stringByDeletingLastPathComponent]];
+		[renameTask setLaunchPath:@"/usr/bin/sed"];
+		NSArray* args = [NSArray arrayWithObjects:@"-ie", regExp, fileName, nil];
+		NSLog(@"ARGS: %@", args);
+		[renameTask setArguments:args];
+		[renameTask launch];
+		[renameTask waitUntilExit];
+	}
+	@catch (NSException *exception) {
+		NSLog(@"ERROR CREATING PROJECT: %@", exception);
+		NSLog(@"working dir: %@", [fileName stringByDeletingLastPathComponent]);
+		[exception raise];
+	}
+	@finally {
+	}
 }
 
 
