d80cb1a2850fddad452f66c09552b0fc404e4ddd
John Twigg <john.twigg@gmail.com>
2014-02-27 16:13:59 -0800

    bug fixes for joints.

--- SpriteBuilder/ccBuilder/SequencerHandler.m
+++ SpriteBuilder/ccBuilder/SequencerHandler.m
@@ -243,16 +243,25 @@ static SequencerHandler* sharedSequencerHandler;
     
     // Expand parents of the selected node
     CCNode* node = [appDelegate.selectedNodes objectAtIndex:0];
-    NSMutableArray* nodesToExpand = [NSMutableArray array];
-    while (node != g.rootNode && node != NULL)
+    
+    
+    if(node.plugIn.isJoint)
     {
-        [nodesToExpand insertObject:node atIndex:0];
-        node = node.parent;
+         [outlineHierarchy expandItem:g.joints];
     }
-    for (int i = 0; i < [nodesToExpand count]; i++)
+    else
     {
-        node = [nodesToExpand objectAtIndex:i];
-        [outlineHierarchy expandItem:node.parent];
+        NSMutableArray* nodesToExpand = [NSMutableArray array];
+        while ((node != g.rootNode || node != g.joints.node) && node != NULL)
+        {
+            [nodesToExpand insertObject:node atIndex:0];
+            node = node.parent;
+        }
+        for (int i = 0; i < [nodesToExpand count]; i++)
+        {
+            node = [nodesToExpand objectAtIndex:i];
+            [outlineHierarchy expandItem:node.parent];
+        }
     }
     
     // Update the selection
