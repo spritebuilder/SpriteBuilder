0ee8ba7c4283eeea1177d7adf572d916856ae5b0
Nicky Weber <nicky@apportable.com>
2014-03-14 10:58:48 +0100

    Saving project settings on quitting app.

--- SpriteBuilder/ccBuilder/AppDelegate.m
+++ SpriteBuilder/ccBuilder/AppDelegate.m
@@ -4699,6 +4699,7 @@ static BOOL hideAllToNextSeparator;
 {
     if ([self windowShouldClose:self])
     {
+		[self.projectSettings store];
         [[NSApplication sharedApplication] terminate:self];
     }
 }
