5574c37a21c8ec8c8e728553b3f7bd3a7bf6b258
Nicky Weber <nicky@apportable.com>
2014-03-20 08:59:10 +0100

    Usage info added to preferences controller.

--- SpriteBuilder/ccBuilder/PreferencesController.h
+++ SpriteBuilder/ccBuilder/PreferencesController.h
@@ -5,6 +5,13 @@
 //  Created by Nicky Weber on 17.03.14.
 //  Copyright (c) 2014 Nicky Weber. All rights reserved.
 //
+//  Usage: Add a NSToolBarItem to the toolbar in the preferences.xib and set an identifier: i.e. <coolitem>
+//         Create a view controller named <coolitem>ViewController
+//
+//  Caveats: Only the height is adjusted to the height of the selected preference window, if a wieder window is needed
+//           Adjust preferences.xib
+//
+//  Todo: Remember last selection
 
 #import <Cocoa/Cocoa.h>
 
