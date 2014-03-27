d918fe981d79e92c5f32d53fd45b17a47ba93d30
Nicky Weber <nicky@apportable.com>
2014-03-27 18:10:41 +0100

    Simplified drawing code for cell's bottom border and reduced size to 1px.

--- SpriteBuilder/ccBuilder/WarningCell.m
+++ SpriteBuilder/ccBuilder/WarningCell.m
@@ -19,28 +19,11 @@
 
     [gc saveGraphicsState];
 
-    CGPoint points[2];
-    points[0].x = 0;
-    points[0].y = cellFrame.origin.y + cellFrame.size.height + 1;
-    points[1].x = 249;
-    points[1].y = cellFrame.origin.y + cellFrame.size.height + 1;
-
+    CGContextSetLineWidth(context, 0.5);
     CGContextSetStrokeColorWithColor(context, [NSColor grayColor].CGColor);
-    CGContextSetLineWidth(context, 1.0);
-
-    CGMutablePathRef mutablePath = CGPathCreateMutable();
-    CGPathAddLines(mutablePath, nil, points, 2);
-
-    CGPathCloseSubpath(mutablePath);
-
-    CGContextBeginPath(context);
-    CGContextAddPath(context, mutablePath);
-    CGContextClosePath(context); //ensure path is closed, not necessary if you know it is
-    CGPathDrawingMode mode = kCGPathStroke;
-    CGContextDrawPath(context, mode);
-
-    CFRelease(mutablePath);
-
+    CGContextMoveToPoint(context, 0.0, cellFrame.origin.y + cellFrame.size.height + 0.5);
+    CGContextAddLineToPoint(context, 249.0, cellFrame.origin.y + cellFrame.size.height + 0.5);
+    CGContextStrokePath(context);
 
     [gc restoreGraphicsState];
 }
