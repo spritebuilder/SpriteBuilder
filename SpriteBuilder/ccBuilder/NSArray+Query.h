7d9e6156515099aa7333e4a54f2144a800ce1963
John Twigg <john.twigg@gmail.com>
2014-04-14 13:31:58 -0700

    update NSArray query.

--- SpriteBuilder/ccBuilder/NSArray+Query.h
+++ SpriteBuilder/ccBuilder/NSArray+Query.h
@@ -8,16 +8,25 @@
 
 #import <Foundation/Foundation.h>
 
-typedef id (^ConvertBlock) (id obj, int idx);
-typedef BOOL (^PredicateBlock) (id obj, int idx);
-typedef void (^VoidBlock) (id obj, int idx);
+typedef id (^ConvertBlock) (id obj, int idx);//Convert obj into a different type.
+typedef BOOL (^PredicateBlock) (id obj, int idx);//Return true if obj conforms to predicate.
+typedef void (^ActionBlock) (id obj, int idx); //Perform an action on obj.
 
 
 @interface NSArray (Query)
 
-
 //Converts all objects in an to a a different type.
 -(NSArray*)convertAll:(ConvertBlock)aBlock;
+
+//Find first item in the list that conforms to the predicate.
 -(id)findFirst:(PredicateBlock)aBlock;
--(void)forEach:(VoidBlock)aBlock;
+
+//Find the last item in the list that conforms to the predicate.
+-(id)findLast:(PredicateBlock)aBlock;
+
+//Return a sublist of items that conform to a predicate.
+-(NSArray*)where:(PredicateBlock)aBlock;
+
+//perform an operation on each item in the list.
+-(void)forEach:(ActionBlock)aBlock;
 @end
