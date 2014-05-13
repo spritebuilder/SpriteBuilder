a91369078d2204845e9a7b3580be1c6374c62035
Nicky Weber <nicky@apportable.com>
2014-05-13 16:07:04 +0200

    Refactoring: Extracted method for preview image generation.

--- SpriteBuilder/libs/Tupac/Tupac.mm
+++ SpriteBuilder/libs/Tupac/Tupac.mm
@@ -434,21 +434,7 @@ typedef struct _PVRTexHeader
         CFRelease(colorSpace);
     }
     
-    // Generate preview file
-    if (self.previewFile)
-    {
-        NSError *error;
-        if (![[NSFileManager defaultManager] removeItemAtPath:self.previewFile error:&error])
-        {
-            NSLog(@"[TEXTUREPACKER] Error removing preview image %@: %@", self.previewFile, error);
-        }
-
-        error = nil;
-        if (![[NSFileManager defaultManager] copyItemAtPath:pngFilename toPath:self.previewFile error:&error])
-        {
-            NSLog(@"[TEXTUREPACKER] Error copying preview image from %@ to %@: %@", pngFilename, self.previewFile, error);
-        }
-    }
+    [self generatePreviewImage:pngFilename];
 
     NSError * error = nil;
 
@@ -542,6 +528,24 @@ typedef struct _PVRTexHeader
         exit(EXIT_FAILURE);
     }
     return result;
+}
+
+- (void)generatePreviewImage:(NSString *)pngFilename
+{
+    if (self.previewFile)
+    {
+        NSError *error;
+        if (![[NSFileManager defaultManager] removeItemAtPath:self.previewFile error:&error])
+        {
+            NSLog(@"[TEXTUREPACKER] Error removing preview image %@: %@", self.previewFile, error);
+        }
+
+        error = nil;
+        if (![[NSFileManager defaultManager] copyItemAtPath:pngFilename toPath:self.previewFile error:&error])
+        {
+            NSLog(@"[TEXTUREPACKER] Error copying preview image from %@ to %@: %@", pngFilename, self.previewFile, error);
+        }
+    }
 }
 
 - (NSArray *) createTextureAtlasFromDirectoryPaths:(NSArray *)dirs
