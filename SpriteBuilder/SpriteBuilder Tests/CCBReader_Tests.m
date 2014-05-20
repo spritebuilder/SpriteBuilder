1f382b7421628eb271ad36cf903e5ba97210dbb9
Nicky Weber <nicky.vault13@gmail.com>
2014-05-20 11:02:09 +0200

    Test target added to SpriteBuilder. First test added to compare written and read CCB versions.

---
+++ SpriteBuilder/SpriteBuilder Tests/CCBReader_Tests.m
@@ -0,0 +1,36 @@
+//
+//  CCBReader.m
+//  SpriteBuilder
+//
+//  Created by Nicky Weber on 20.05.14.
+//
+//
+
+#import <XCTest/XCTest.h>
+#import "CCBXCocos2diPhoneWriter.h"
+#import "CCBReader_Private.h"
+
+@interface CCBReader_Tests : XCTestCase
+
+@end
+
+@implementation CCBReader_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testCCBVersionCompatibility
+{
+    XCTAssertEqual(kCCBVersion, kCCBXVersion, @"CCB version %d read by CCBReader is incompatible with version %d written by SpriteBuilder. Is cocos2d up to date?", kCCBVersion, kCCBXVersion);
+}
+
+@end
