2da60edad83311725a4626bf364789fb14a6b574
Nicky Weber <nicky.vault13@gmail.com>
2014-05-22 15:26:41 +0200

    New package window: Input validation points added.

--- SpriteBuilder/ccBuilder/NewPackageWindowController.m
+++ SpriteBuilder/ccBuilder/NewPackageWindowController.m
@@ -7,6 +7,7 @@
 //
 
 #import "NewPackageWindowController.h"
+#import "PackageCreateDelegate.h"
 
 @interface NewPackageWindowController ()
 
@@ -29,18 +30,45 @@
     return self;
 }
 
+- (void)controlTextDidChange:(NSNotification *)obj
+{
+    [self resetWarning];
+}
+
 - (IBAction)onCreate:(id)sender
 {
+    NSError *error;
+    if (![_delegate canCreatePackageWithName:_packageName error:&error])
+    {
+        [self showCannotCreatePackageWarningWithError:error];
+        return;
+    }
+
     [NSApp stopModalWithCode:1];
 }
+
+- (void)showCannotCreatePackageWarningWithError:(NSError *)error
+{
+    _errorMessage.hidden = NO;
+    _errorMessage.stringValue = error.localizedDescription;
+    _packageNameInput.textColor = [NSColor colorWithSRGBRed:1.0 green:0.0 blue:0.0 alpha:1.0];
+}
+
+- (void)resetWarning
+{
+    _errorMessage.hidden = YES;
+    _packageNameInput.textColor = [NSColor blackColor];
+}
+
 - (IBAction)onCancel:(id)sender
 {
     [NSApp stopModalWithCode:0];
 }
+
 - (void)windowDidLoad
 {
     [super windowDidLoad];
-    
+
     // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
 }
 
