74f3edbf90c09ea7d0f39f4a47daf82f154ba0c3
Nicky Weber <nicky.vault13@gmail.com>
2014-05-27 09:22:59 +0200

    OCMock framework added to test target. Test running in Xcode and AppCode. Skeleton test class added for Packages.

---
+++ SpriteBuilder/SpriteBuilder Tests/PackageController_Tests.m
@@ -0,0 +1,35 @@
+//
+//  PackageController_Test.m
+//  SpriteBuilder
+//
+//  Created by Nicky Weber on 26.05.14.
+//
+//
+
+#import <XCTest/XCTest.h>
+#import <OCMock/OCMock.h>
+
+@interface PackageController_Tests : XCTestCase
+
+@end
+
+@implementation PackageController_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTAssertEqual(1, 1, @"---");
+}
+
+@end
