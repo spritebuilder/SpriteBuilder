4adfcc781e52a810927ebd59cdde14b6068b8fb8
Nicky Weber <nicky.vault13@gmail.com>
2014-05-27 14:39:49 +0200

    PackageController creation tests added.

--- SpriteBuilder/SpriteBuilder Tests/PackageController_Tests.m
+++ SpriteBuilder/SpriteBuilder Tests/PackageController_Tests.m
@@ -8,28 +8,71 @@
 
 #import <XCTest/XCTest.h>
 #import <OCMock/OCMock.h>
+#import <objc/runtime.h>
+#import "PackageCreateDelegateProtocol.h"
+#import "PackageController.h"
+#import "ProjectSettings.h"
+#import "SBErrors.h"
+
 
 @interface PackageController_Tests : XCTestCase
 
 @end
 
 @implementation PackageController_Tests
+{
+    PackageController *_packageController;
+    id _projectSettingsMock;
+    id _fileManagerMock;
+}
 
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    _packageController = [[PackageController alloc] init];
+
+    _projectSettingsMock = [OCMockObject mockForClass:[ProjectSettings class]];
+    _packageController.projectSettings = _projectSettingsMock;
+
+    _fileManagerMock = [OCMockObject mockForClass:[NSFileManager class]];
+    _packageController.fileManager = _fileManagerMock;
 }
 
 - (void)tearDown
 {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testCreatePackageWithName
 {
-    XCTAssertEqual(1, 1, @"---");
+    [[[_projectSettingsMock expect] andReturn:@"/"] projectPathDir];
+    [[[_projectSettingsMock expect] andReturnValue:@(NO)] isResourcePathAlreadyInProject:@"/NewPackage.sbpack"];
+    [[[_projectSettingsMock expect] andReturnValue:@(YES)] addResourcePath:@"/NewPackage.sbpack" error:[OCMArg anyObjectRef]];
+
+    [[[_fileManagerMock expect] andReturnValue:@(YES)] createDirectoryAtPath:@"/NewPackage.sbpack"
+                                                 withIntermediateDirectories:NO
+                                                                  attributes:nil
+                                                                       error:[OCMArg anyObjectRef]];
+    NSError *error;
+    XCTAssertTrue([_packageController createPackageWithName:@"NewPackage" error:&error], @"Creation of package should return YES.");
+    XCTAssertNil(error, @"Error object should nil");
+
+    [_projectSettingsMock verify];
+    [_fileManagerMock verify];
+}
+
+- (void)testCreatePackageWithNameAlreadyInProject
+{
+    [[[_projectSettingsMock expect] andReturn:@"/"] projectPathDir];
+    [[[_projectSettingsMock expect] andReturnValue:@(YES)] isResourcePathAlreadyInProject:@"/NewPackage.sbpack"];
+
+    NSError *error;
+    XCTAssertFalse([_packageController createPackageWithName:@"NewPackage" error:&error], @"Creation of package should return NO.");
+    XCTAssertNotNil(error, @"Error object should be set");
+    XCTAssertEqual(error.code, SBDuplicateResourcePathError, @"Error code should equal constant SBDuplicateResourcePathError");
+
+    [_projectSettingsMock verify];
 }
 
 @end
