d1b103e18c8e512ada13e7719599889f30e66026
Nicky Weber <nicky.vault13@gmail.com>
2014-06-20 11:29:01 +0200

    Refactoring: Splitting up lengthy method.

--- SpriteBuilder/ccBuilder/ResourceDeleteCommand.m
+++ SpriteBuilder/ccBuilder/ResourceDeleteCommand.m
@@ -16,20 +16,9 @@
     NSAssert(_projectSettings != nil ,@"Project settings must not be nil");
     NSAssert(_outlineView != nil ,@"OutlineView should be set");
 
-    if (!_resources || _resources.count == 0)
-    {
-        return;
-    }
-
-    NSAlert *alert = [NSAlert alertWithMessageText:@"Are you sure you want to delete the selected files?"
-                                     defaultButton:@"Cancel"
-                                   alternateButton:@"Delete"
-                                       otherButton:NULL
-                         informativeTextWithFormat:@"You cannot undo this operation."];
-
-    NSInteger result = [alert runModal];
-
-    if (result == NSAlertDefaultReturn)
+    if (!_resources
+        || _resources.count == 0
+        || ![self askUserForConfirmation])
     {
         return;
     }
@@ -38,6 +27,41 @@
     NSMutableArray *foldersToDelete = [[NSMutableArray alloc] init];
     NSMutableArray *packagesPathsToDelete = [[NSMutableArray alloc] init];
 
+    [self sortItemsToDeleteIntoArrays:resourcesToDelete
+                      foldersToDelete:foldersToDelete
+                packagesPathsToDelete:packagesPathsToDelete];
+
+    [self deleteResourcesInArrays:resourcesToDelete
+                  foldersToDelete:foldersToDelete
+            packagesPathsToDelete:packagesPathsToDelete];
+
+    [_outlineView deselectAll:nil];
+
+    // TODO: inject with property
+    [[ResourceManager sharedManager] reloadAllResources];
+}
+
+- (void)deleteResourcesInArrays:(NSMutableArray *)resourcesToDelete
+                foldersToDelete:(NSMutableArray *)foldersToDelete
+          packagesPathsToDelete:(NSMutableArray *)packagesPathsToDelete
+{
+    for (RMResource *res in resourcesToDelete)
+    {
+        [ResourceManager removeResource:res];
+    }
+
+    for (RMResource *res in foldersToDelete)
+    {
+        [ResourceManager removeResource:res];
+    }
+
+    PackageRemover *packageRemover = [[PackageRemover alloc] init];
+    packageRemover.projectSettings = _projectSettings;
+    [packageRemover removePackagesFromProject:packagesPathsToDelete error:NULL];
+}
+
+- (void)sortItemsToDeleteIntoArrays:(NSMutableArray *)resourcesToDelete foldersToDelete:(NSMutableArray *)foldersToDelete packagesPathsToDelete:(NSMutableArray *)packagesPathsToDelete
+{
     for (id resource in _resources)
     {
         if ([resource isKindOfClass:[RMResource class]])
@@ -59,24 +83,19 @@
             [packagesPathsToDelete addObject:rmDirectory.dirPath];
         }
     }
+}
 
-    for (RMResource *res in resourcesToDelete)
-    {
-        [ResourceManager removeResource:res];
-    }
+- (BOOL)askUserForConfirmation
+{
+    NSAlert *alert = [NSAlert alertWithMessageText:@"Are you sure you want to delete the selected files?"
+                                     defaultButton:@"Cancel"
+                                   alternateButton:@"Delete"
+                                       otherButton:NULL
+                         informativeTextWithFormat:@"You cannot undo this operation."];
 
-    for (RMResource *res in foldersToDelete)
-    {
-        [ResourceManager removeResource:res];
-    }
+    NSInteger result = [alert runModal];
 
-    PackageRemover *packageRemover = [[PackageRemover alloc] init];
-    packageRemover.projectSettings = _projectSettings;
-    [packageRemover removePackagesFromProject:packagesPathsToDelete error:NULL];
-
-    [_outlineView deselectAll:nil];
-
-    [[ResourceManager sharedManager] reloadAllResources];
+    return result != NSAlertDefaultReturn;
 }
 
 
