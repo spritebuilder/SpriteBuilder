9d07bf7eda73752cf38258f40486e88c607481e7
Nicky Weber <nicky.vault13@gmail.com>
2014-06-24 17:14:01 +0200

    PackageMigration first test added. Assertion helpers added. Removed calls to super, not needed.

--- SpriteBuilder/SpriteBuilder Tests/Migration/PackageMigrator_Tests.m
+++ SpriteBuilder/SpriteBuilder Tests/Migration/PackageMigrator_Tests.m
@@ -10,6 +10,8 @@
 #import "PackageMigrator.h"
 #import "ProjectSettings.h"
 #import "FileSystemTestCase.h"
+#import "NSString+Packages.h"
+#import "ProjectSettings+Packages.h"
 
 @interface PackageMigrator_Tests : FileSystemTestCase
 
@@ -17,26 +19,71 @@
 
 @implementation PackageMigrator_Tests
 
-- (void)setUp
+#pragma mark - setup
+
+- (ProjectSettings *)createProjectSettingsWithResourcePaths:(NSArray *)resourcePaths
 {
-    [super setUp];
+    NSString *projectSettingsPath = [self fullPathForFile:@"migrationtest.ccbproj"];
+
+    NSMutableDictionary *projectDict = [NSMutableDictionary dictionaryWithContentsOfFile:projectSettingsPath];
+    ProjectSettings *projectSettings = [[ProjectSettings alloc] initWithSerialization:projectDict];
+    projectSettings.projectPath = projectSettingsPath;
+
+    for (NSString *resourcePath in resourcePaths)
+    {
+        [projectSettings addResourcePath:[self fullPathForFile:resourcePath] error:nil];
+    }
+
+    return projectSettings;
 }
 
-- (void)tearDown
-{
-    [super tearDown];
-}
 
-- (void)testExample
+#pragma mark - tests
+
+- (void)testMigrationStandardCaseNoPackageFolderNoPackages
 {
     [self createFolders:@[@"SpriteBuilder Resources"]];
+
     [self createProjectSettingsFileWithName:@"migrationtest"];
+    [self assertFileExists:@"migrationtest.ccbproj"];
 
     [self createEmptyFiles:@[
             @"SpriteBuilder Resources/asset.png",
             @"SpriteBuilder Resources/scene.ccb"]];
 
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    ProjectSettings *projectSettings = [self createProjectSettingsWithResourcePaths:@[@"SpriteBuilder Resources"]];
+
+    PackageMigrator *packageMigrator = [[PackageMigrator alloc] initWithProjectSettings:projectSettings];
+
+    NSError *error;
+    XCTAssertTrue([packageMigrator migrate:&error], @"Migration failed, error: %@", error);
+
+    [self assertFileExists:@"packages"];
+    [self assertFileDoesNotExists:@"SpriteBuilder Resources"];
+    [self assertFileExists:[@"packages/SpriteBuilder Resources" stringByAppendingPackageSuffix]];
+    [self assertResourcePaths:@[[projectSettings fullPathForPackageName:@"SpriteBuilder Resources"]]
+            inProjectSettings:projectSettings];
+    [self assertResourcePaths:@[[self fullPathForFile:@"SpriteBuilder Resources"]]
+            notInProjectSettings:projectSettings];
+}
+
+
+#pragma mark - assertion helper
+
+- (void)assertResourcePaths:(NSArray *)resourcePaths inProjectSettings:(ProjectSettings *)projectSettings
+{
+    for (NSString *resourcePath in resourcePaths)
+    {
+        XCTAssertTrue([projectSettings isResourcePathInProject:resourcePath], @"Resource path \"%@\"is not in project settings. Found in settings: %@", resourcePath, projectSettings.resourcePaths);
+    }
+}
+
+- (void)assertResourcePaths:(NSArray *)resourcePaths notInProjectSettings:(ProjectSettings *)projectSettings
+{
+    for (NSString *resourcePath in resourcePaths)
+    {
+        XCTAssertFalse([projectSettings isResourcePathInProject:resourcePath], @"Resource path \"%@\"is in project settings.");
+    }
 }
 
 @end
