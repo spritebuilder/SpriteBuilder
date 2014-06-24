cb5eb2543a9663d6e5d7919a0b861aba4d9184e3
Nicky Weber <nicky.vault13@gmail.com>
2014-06-24 14:01:37 +0200

    Package migration for standard case working. No .sbpack folders present nor a packages/ folder.

--- SpriteBuilder/ccBuilder/PackageMigrator.m
+++ SpriteBuilder/ccBuilder/PackageMigrator.m
@@ -2,7 +2,8 @@
 
 #import "ProjectSettings.h"
 #import "ProjectSettings+Packages.h"
-#import "MiscConstants.h"
+#import "NSString+Packages.h"
+#import "PackageImporter.h"
 
 #define LocalLogDebug( s, ... ) NSLog( @"[DEBUG] <%@:%d> %@", [[NSString stringWithUTF8String:__FILE__] lastPathComponent], __LINE__,  [NSString stringWithFormat:(s), ##__VA_ARGS__] )
 #define LocalLogError( s, ... ) NSLog( @"[ERROR] <%@:%d> %@", [[NSString stringWithUTF8String:__FILE__] lastPathComponent], __LINE__,  [NSString stringWithFormat:(s), ##__VA_ARGS__] )
@@ -35,19 +36,68 @@
 {
     LocalLogDebug(@"Package migration started...");
 
-    if (![self renameResourcePathWithPackagesFolderName:error])
+/*    if (![self renameResourcePathWithPackagesFolderName:error])
     {
         return NO;
     }
+
+
+    if (![self migrateAllResourcePaths:error])
+    {
+        return NO;
+    }*/
+
 
     if (![self createPackagesFolderIfNotExisting:NULL])
     {
         return NO;
     }
 
-    if (![self migrateAllResourcePaths:error])
+    NSMutableArray *resourcePathsToImport = [NSMutableArray array];
+    for (NSMutableDictionary *resourcePathDict in [_projectSettings.resourcePaths copy])
+    {
+        NSString *fullResourcePath = [_projectSettings fullPathForResourcePathDict:resourcePathDict];
+        if ([_projectSettings isPathInPackagesFolder:fullResourcePath])
+        {
+            continue;
+        }
+
+        [resourcePathsToImport addObject:[fullResourcePath mutableCopy]];
+        if (![_projectSettings removeResourcePath:fullResourcePath error:error])
+        {
+            return NO;
+        }
+    }
+
+    for (NSMutableString *fullPath in resourcePathsToImport)
+    {
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+
+        NSString *oldPath = fullPath;
+        NSString *newPath = [fullPath stringByAppendingPackageSuffix];
+
+        if (![fileManager moveItemAtPath:oldPath toPath:newPath error:error])
+        {
+            return NO;
+        }
+
+        [fullPath setString:newPath];
+    }
+
+    PackageImporter *packageImporter = [[PackageImporter alloc] init];
+    packageImporter.projectSettings = _projectSettings;
+    if (![packageImporter importPackagesWithPaths:resourcePathsToImport error:error])
     {
         return NO;
+    }
+
+    for (NSMutableString *fullPath in resourcePathsToImport)
+    {
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+        if (![fileManager removeItemAtPath:fullPath error:error])
+        {
+            return NO;
+        }
     }
 
     LocalLogDebug(@"Package finished successfully!");
@@ -82,6 +132,16 @@
     return YES;
 }
 
+- (BOOL)packageFolderExists
+{
+    NSString *packageFolderPath = [_projectSettings packagesFolderPath];
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+
+    return [fileManager fileExistsAtPath:packageFolderPath];
+}
+
+
+/*
 - (BOOL)renameResourcePathWithPackagesFolderName:(NSError **)error
 {
     if ([self packageFolderExists]
@@ -92,13 +152,6 @@
     return YES;
 }
 
-- (BOOL)packageFolderExists
-{
-    NSString *packageFolderPath = [_projectSettings packagesFolderPath];
-    NSFileManager *fileManager = [NSFileManager defaultManager];
-
-    return [fileManager fileExistsAtPath:packageFolderPath];
-}
 
 - (BOOL)isPackageFolderAResourcePath
 {
@@ -131,6 +184,17 @@
     LocalLogDebug(@"Special case: resource path with name \"%@\" found: renaming to: \"%@\"", PACKAGES_FOLDER_NAME, renamePathTo);
     LocalLogDebug(@"Trying to rename resource folder with \"packages\" name DONE");
 
+    NSString *newResourcePathName = [renamePathTo lastPathComponent];
+    for (NSMutableDictionary *resourcePath in _projectSettings.resourcePaths)
+    {
+        if ([[_projectSettings fullPathForResourcePathDict:resourcePath] isEqualToString:[_projectSettings packagesFolderPath]])
+        {
+            // TODO: use ResourcePath object
+            resourcePath[@"path"] = [[resourcePath[@"path"] stringByDeletingLastPathComponent] stringByAppendingPathComponent:newResourcePathName];
+            LocalLogDebug(@"New relative path: \"%@\"", [_projectSettings fullPathForResourcePathDict:resourcePath]);
+        }
+    }
+
     return YES;
 }
 
@@ -153,28 +217,104 @@
 - (BOOL)migrateAllResourcePaths:(NSError **)error
 {
     LocalLogDebug(@"Migrating resource paths...");
-    for (NSMutableDictionary *resourcePath in _projectSettings.resourcePaths)
+    for (NSMutableDictionary *resourcePathDict in _projectSettings.resourcePaths)
     {
-        LocalLogDebug(@"Current resource path %@", resourcePath[@"path"]);
+        ResourcePath *resourcePath = [[ResourcePath alloc] initWithDictionary:resourcePathDict];
 
+        LocalLogDebug(@"Current resource path \"%@\"", resourcePath.relativePath);
+        if ([self isResourcePathInPackagesFolder:resourcePath])
+        {
+            LocalLogDebug(@"Skipping: Resource path \"%@\" already in packages folder.", resourcePath);
+            continue;
+        }
 
+        if (![self moveResourcePathToPackagesFolder:resourcePath error:error])
+        {
+            return NO;
+        }
 
+        resourcePathDict[@"path"] = resourcePath.relativePath;
+        LocalLogDebug(@"Resource path moved to rel path \"%@\"", resourcePath.relativePath);
     }
+    return YES;
 }
 
-- (BOOL)isResourcePathInPackagesFolder:(NSMutableDictionary *)resourcePath
+- (BOOL)isResourcePathInPackagesFolder:(ResourcePath *)resourcePath
 {
+    NSString *fullResourcePath = [_projectSettings fullPathForResourcePath:resourcePath];
 
+    return [_projectSettings isPathInPackagesFolder:fullResourcePath];
 }
 
-- (BOOL)renameResourcePathBeforeMovingToPackage:(NSMutableDictionary *)resourcePath
+- (BOOL)renameResourcePathBeforeMovingToPackage:(ResourcePath *)resourcePath error:(NSError **)error
 {
-    // Use map?
+    // Use map? / History for rollback?
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+
+    NSString *fullResourcePath = [_projectSettings fullPathForResourcePath:resourcePath];
+    NSString *folderNameOfResource = [resourcePath.relativePath lastPathComponent];
+    NSString *futureFullPathInPackagesFolder = [_projectSettings fullPathForPackageName:folderNameOfResource];
+
+    // TODO melt with renamePathForSpecialCasePackagesFolderAsResourcePath
+    NSUInteger count = 0;
+    while ([fileManager fileExistsAtPath:futureFullPathInPackagesFolder])
+    {
+        LocalLogDebug(@"Existing folder in packages/ for moving resource path. Name \"%@\" exists, trying next...", [futureFullPathInPackagesFolder lastPathComponent]);
+
+        NSString *renameSuffixWithCount =[NSString stringWithFormat:@"%@.%lu", @"renamed", count];
+        futureFullPathInPackagesFolder = [[_projectSettings packagesFolderPath] stringByAppendingPathExtension:renameSuffixWithCount];
+        count ++;
+    }
+
+    if (![fileManager moveItemAtPath:fullResourcePath toPath:futureFullPathInPackagesFolder error:error])
+    {
+        LocalLogDebug(@"EROR renaming \"%@\" to \"%@\"", fullResourcePath, futureFullPathInPackagesFolder);
+        return NO;
+    }
+
+    NSString *newResourcePathName = [futureFullPathInPackagesFolder lastPathComponent];
+    resourcePath.relativePath = [[resourcePath.relativePath stringByDeletingLastPathComponent] stringByAppendingPathComponent:newResourcePathName];
+    LocalLogDebug(@"New relative path: \"%@\"", fullResourcePath);
+
+    return YES;
 }
 
-- (BOOL)moveResourcePathToPackagesFolder:(NSString *)resourcePath
+- (BOOL)moveResourcePathToPackagesFolder:(ResourcePath *)resourcePath error:(NSError **)error
 {
+    LocalLogDebug(@"Trying to move resource path \"%@\" to packages/ folder", resourcePath);
+    if (![self canMoveResourcePathToPackagesFolder:resourcePath])
+    {
+        LocalLogDebug(@"Cannot move, name collision, trying to rename: \"%@\"", resourcePath);
+        if (![self renameResourcePathBeforeMovingToPackage:resourcePath error:error])
+        {
+            return NO;
+        }
+    }
 
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+
+    NSString *fromPath = [_projectSettings fullPathForResourcePath:resourcePath];
+    NSString *packageName = [resourcePath.relativePath lastPathComponent];
+    NSString *toPath = [_projectSettings fullPathForPackageName:packageName];
+
+    if (![fileManager moveItemAtPath:fromPath toPath:toPath error:error])
+    {
+        return NO;
+    }
+
+    resourcePath.relativePath = [PACKAGES_FOLDER_NAME stringByAppendingPathComponent:packageName];
+
+    return YES;
+}
+
+- (BOOL)canMoveResourcePathToPackagesFolder:(ResourcePath *)resourcePath
+{
+    NSString *folderNameOfResource = [resourcePath.relativePath lastPathComponent];
+    NSString *futureFullPathInPackagesFolder = [_projectSettings fullPathForPackageName:folderNameOfResource];
+
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+
+    return ![fileManager fileExistsAtPath:futureFullPathInPackagesFolder];
 }
 
 - (BOOL)restoreSpecialCasePackagesResourcePathFolder
@@ -185,13 +325,15 @@
     {
         // Restore name
     }
+
+    return YES;
 }
 
 - (BOOL)addPackageSuffixToMovedResourcePaths:(NSMutableDictionary *)resourcePath
 {
-
+    return YES;
 }
 
-
+*/
 
 @end
