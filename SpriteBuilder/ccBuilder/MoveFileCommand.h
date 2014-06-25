2a0e03cd473bfebf37cfbc2f404598cdf2733938
Nicky Weber <nicky.vault13@gmail.com>
2014-06-25 13:34:12 +0200

    FileCommands interface added.

--- SpriteBuilder/ccBuilder/MoveFileCommand.h
+++ SpriteBuilder/ccBuilder/MoveFileCommand.h
@@ -1,17 +1,13 @@
 #import <Foundation/Foundation.h>
+#import "FileCommandProtocol.h"
 
-@interface MoveFileCommand : NSObject
+@interface MoveFileCommand : NSObject <FileCommandProtocol>
 
 @property (nonatomic, copy) NSString *fromPath;
 @property (nonatomic, copy) NSString *toPath;
 
 @property (nonatomic, readonly) BOOL successful;
 @property (nonatomic, strong, readonly) NSError *error;
-
 - (instancetype)initWithFromPath:(NSString *)fromPath toPath:(NSString *)toPath;
-
-- (BOOL)execute:(NSError **)error;
-
-- (BOOL)undo:(NSError **)error;
 
 @end
