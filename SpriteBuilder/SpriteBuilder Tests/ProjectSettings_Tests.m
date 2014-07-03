874c938014326609e7215af852a3585804c20454
Nicky Weber <nicky.vault13@gmail.com>
2014-07-03 12:03:54 +0200

    ProjectSettings simple tests added. Loading from a dictionary and some values that get migrated/created if not present.

--- SpriteBuilder/SpriteBuilder Tests/ProjectSettings_Tests.m
+++ SpriteBuilder/SpriteBuilder Tests/ProjectSettings_Tests.m
@@ -144,4 +144,199 @@
     SBAssertStringsEqual(fullPackagesPath, _projectSettings.packagesFolderPath);
 }
 
+- (void)testInitWithDictionary
+{
+   NSDictionary *projectDict =
+   @{
+      @"deviceOrientationPortrait":@(NO),
+      @"publishDirectory":@"Source/Resources/Published-iOS",
+      @"resourceAutoScaleFactor":@(0),
+      @"flattenPaths":@(NO),
+      @"resourceProperties":@{
+         @"":@{
+            @"previewFolderHidden":@(YES)
+         },
+         @"ccbResources/ccbSliderBgHighlighted.png":@{
+            @"tabletScale":@(1),
+            @"scaleFrom":@(2)
+         },
+         @"ccbResources/ccbButtonHighlighted.png":@{
+            @"tabletScale":@(1),
+            @"scaleFrom":@(2)
+         },
+         @"Sprites" : @{},
+         @"ccbResources/ccbSliderBgNormal.png":@{
+            @"tabletScale":@(1),
+            @"scaleFrom":@(2)
+         },
+         @"ccbResources/ccbTextField.png":@{
+            @"tabletScale":@(1),
+            @"scaleFrom":@(2)
+         },
+         @"ccbResources/ccbParticleFire.png":@{
+            @"tabletScale":@(1),
+            @"scaleFrom":@(1)
+         },
+         @"ccbResources":@{
+            @"previewFolderHidden":@(YES)
+         },
+         @"ccbResources/ccbParticleMagic.png":@{
+            @"tabletScale":@(1),
+            @"scaleFrom":@(1)
+         },
+         @"ccbResources/ccbButtonNormal.png":@{
+            @"tabletScale":@(1),
+            @"scaleFrom":@(2)
+         },
+         @"ccbResources/ccbParticleStars.png":@{
+            @"tabletScale":@(1),
+            @"scaleFrom":@(1)
+         },
+         @"ccbResources/ccbSliderHandle.png":@{
+            @"tabletScale":@(1),
+            @"scaleFrom":@(2)
+         },
+         @"ccbResources/ccbParticleSmoke.png":@{
+            @"tabletScale":@(1),
+            @"scaleFrom":@(1)
+         },
+         @"ccbResources/ccbParticleSnow.png":@{
+            @"tabletScale":@(1),
+            @"scaleFrom":@(1)
+         }
+      },
+      @"publishDirectoryAndroid":@"Source/Resources/Published-Android",
+      @"defaultOrientation":@(0),
+      @"publishResolution_android_tablet":@(YES),
+      @"publishEnabledAndroid":@(YES),
+      @"publishResolution_ios_phonehd":@(YES),
+      @"publishResolution_ios_tablet":@(YES),
+      @"publishResolution_android_phone":@(YES),
+      @"fileType":@"CocosBuilderProject",
+      @"resourcePaths":@[
+         @{
+            @"path":@"packages/SpriteBuilder Resources.sbpack"
+         }
+      ],
+      @"publishAudioQuality_android":@(4),
+      @"deviceOrientationLandscapeLeft":@(YES),
+      @"publishResolution_android_tablethd":@(YES),
+      @"publishAudioQuality_ios":@(4),
+      @"publishEnvironment":@(0),
+      @"publishEnablediPhone":@(YES),
+      @"publishToZipFile":@(NO),
+      @"exporter":@"ccbi",
+      @"versionStr":@"Version: 1.x\n-n GitHub: \nfcec170fc2\n",
+      @"publishResolution_ios_phone":@(YES),
+      @"publishResolution_ios_tablethd":@(YES),
+      @"deviceOrientationUpsideDown":@(NO),
+      @"publishResolution_android_phonehd":@(YES),
+      @"deviceOrientationLandscapeRight":@(YES),
+      @"onlyPublishCCBs":@(NO),
+      @"deviceScaling":@(0),
+      @"designTarget":@(0),
+      @"cocos@(2)dUpdateIgnoredVersions":@[],
+      @"engine":@(0)
+   };
+
+    ProjectSettings *project = [[ProjectSettings alloc] initWithSerialization:projectDict];
+
+    XCTAssertFalse(project.deviceOrientationPortrait);
+    XCTAssertFalse(project.deviceOrientationUpsideDown);
+    XCTAssertTrue(project.deviceOrientationLandscapeLeft);
+    XCTAssertTrue(project.deviceOrientationLandscapeRight);
+
+    // This a convention, if it's read as 0 has to become 4
+    XCTAssertEqual(project.resourceAutoScaleFactor, 4);
+    SBAssertStringsEqual(project.publishDirectory, @"Source/Resources/Published-iOS");
+    XCTAssertFalse(project.flattenPaths);
+
+    SBAssertStringsEqual(project.publishDirectoryAndroid, @"Source/Resources/Published-Android");
+    XCTAssertEqual(project.defaultOrientation, 0);
+
+    XCTAssertTrue(project.publishEnabledAndroid);
+    XCTAssertTrue(project.publishResolution_android_phone);
+    XCTAssertTrue(project.publishResolution_android_phonehd);
+    XCTAssertTrue(project.publishResolution_android_tablet);
+    XCTAssertTrue(project.publishResolution_android_tablethd);
+
+    XCTAssertTrue(project.publishEnablediPhone);
+    XCTAssertTrue(project.publishResolution_ios_phone);
+    XCTAssertTrue(project.publishResolution_ios_phonehd);
+    XCTAssertTrue(project.publishResolution_ios_tablet);
+    XCTAssertTrue(project.publishResolution_ios_tablethd);
+
+    [self assertResourcePaths:@[@"packages/SpriteBuilder Resources.sbpack"] inProject:project];
+
+    XCTAssertEqual(project.publishAudioQuality_android, 4);
+    XCTAssertEqual(project.publishAudioQuality_ios, 4);
+
+    XCTAssertFalse(project.onlyPublishCCBs);
+    XCTAssertEqual(project.deviceScaling, 0);
+    XCTAssertEqual(project.designTarget, 0);
+
+    XCTAssertEqual(project.engine, CCBTargetEngineCocos2d);
+    SBAssertStringsEqual(project.exporter, @"ccbi");
+
+    XCTAssertFalse(project.publishToZipFile);
+    XCTAssertEqual(project.publishEnvironment, PublishEnvironmentDevelop);
+
+    NSNumber *scaleFrom = [project valueForRelPath:@"ccbResources/ccbSliderBgNormal.png" andKey:@"scaleFrom"];
+    XCTAssertTrue([scaleFrom isEqualToNumber:@(2)]);
+}
+
+- (void)testWrongFileType
+{
+    NSDictionary *projectDict =
+    @{
+        @"fileType" : @"fooooo"
+    };
+
+    ProjectSettings *project = [[ProjectSettings alloc] initWithSerialization:projectDict];
+    XCTAssertNil(project);
+}
+
+- (void)testWithSomeMissingKeyAndValues
+{
+    NSDictionary *projectDict =
+    @{
+       @"fileType":@"CocosBuilderProject"
+    };
+
+    ProjectSettings *project = [[ProjectSettings alloc] initWithSerialization:projectDict];
+    XCTAssertNotNil(project);
+    XCTAssertEqual(project.publishAudioQuality_android, 1);
+    XCTAssertEqual(project.publishAudioQuality_ios, 1);
+    SBAssertStringsEqual(project.publishDirectory, @"");
+    SBAssertStringsEqual(project.publishDirectoryAndroid, @"");
+}
+
+// This test exists to ensure noone changes values by mistake that are persisted and have to
+// be migrated with more effort to fix this change
+- (void)testEnums
+{
+    XCTAssertEqual(PublishEnvironmentDevelop, 0);
+    XCTAssertEqual(PublishEnvironmentRelease, 1);
+
+    XCTAssertEqual(CCBTargetEngineCocos2d, 0);
+    XCTAssertEqual(CCBTargetEngineSpriteKit, 1);
+
+    XCTAssertEqual(kCCBOrientationLandscape, 0);
+    XCTAssertEqual(kCCBOrientationPortrait, 1);
+
+    XCTAssertEqual(kCCBDesignTargetFlexible, 0);
+    XCTAssertEqual(kCCBDesignTargetFixed, 1);
+}
+
+
+#pragma mark - test helper
+
+- (void)assertResourcePaths:(NSArray *)resourcePaths inProject:(ProjectSettings *)project
+{
+    for (NSString *resourcePath in resourcePaths)
+    {
+        XCTAssertTrue([project isResourcePathInProject:resourcePath], @"Resource path \"%@\"is not in project settings. Found in settings: %@", resourcePath, _projectSettings.resourcePaths);
+    }
+}
+
 @end
