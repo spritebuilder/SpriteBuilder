21923b6e3e6469d4498efedb64915abad80e7b94
Nicky Weber <nicky.vault13@gmail.com>
2014-08-07 22:26:41 +0200

    Refactoring: Cocos2dUpdater+Errors category removed. It wasn't really helping keeping the class small. Another refactoring is needed to split the Cocos2dUpdater class into more logical parts if possible.

--- SpriteBuilder/ccBuilder/Cocos2dUpdater.m
+++ SpriteBuilder/ccBuilder/Cocos2dUpdater.m
@@ -675,4 +675,41 @@ static int copyFileCallback(int currentState, int stage, copyfile_state_t state,
     self.cancelled = YES;
 }
 
+- (NSError *)errorForFailedUnzipTask:(NSString *)zipFile dataStdErr:(NSData *)dataStdErr status:(int)status
+{
+    NSString *stdErr = [[NSString alloc] initWithData: dataStdErr encoding: NSUTF8StringEncoding];
+
+    NSDictionary *userInfo = @{
+            @"zipFile" : zipFile,
+            @"stdErr" : stdErr,
+            NSLocalizedDescriptionKey : [NSString stringWithFormat:@"Unzip task exited with status code %d. See stdErr key in userInfo.", status]};
+
+    return [NSError errorWithDomain:SBErrorDomain
+                               code:SBCocos2dUpdateUnzipTemplateFailedError
+                           userInfo:userInfo];
+}
+
+- (NSError *)errorForUnzipTaskWithException:(NSException *)exception zipFile:(NSString *)zipFile
+{
+    NSDictionary *userInfo = @{
+            @"zipFile" : zipFile,
+            @"exception" : exception,
+            NSLocalizedDescriptionKey : [NSString stringWithFormat:@"Exception %@ thrown while running unzip task.", exception.name]};
+
+    return [NSError errorWithDomain:SBErrorDomain
+                                     code:SBCocos2dUpdateUnzipTaskError
+                                 userInfo:userInfo];
+}
+
+- (NSError *)errorForNonExistentTemplateFile:(NSString *)zipFile
+{
+    NSDictionary *userInfo = @{
+            @"zipFile" : zipFile,
+            NSLocalizedDescriptionKey : @"Project template zip file does not exist, unable to extract newer cocos2d version."};
+
+    return [NSError errorWithDomain:SBErrorDomain
+                                     code:SBCocos2dUpdateTemplateZipFileDoesNotExistError
+                                 userInfo:userInfo];
+}
+
 @end
