f4598422dad81991f59cc46779d8c452c6004254
Collin Jackson <jackson@apportable.com>
2014-10-06 17:38:29 -0700

    move template project's files into the correct place for issue 888 (adding the Mac target). Mac target doesn't work yet

---
+++ Support/PROJECTNAME.spritebuilder/Source/Platforms/Mac/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  PROJECTNAME Mac
+//
+//  Created by Collin Jackson on 10/2/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
