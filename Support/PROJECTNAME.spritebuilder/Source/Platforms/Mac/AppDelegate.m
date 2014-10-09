5938db2e65682d6da2c62c4771f6d84cefcd18d5
Collin Jackson <jackson@apportable.com>
2014-10-08 18:33:07 -0700

    Fix drawing for Mac target

--- Support/PROJECTNAME.spritebuilder/Source/Platforms/Mac/AppDelegate.m
+++ Support/PROJECTNAME.spritebuilder/Source/Platforms/Mac/AppDelegate.m
@@ -10,76 +10,32 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Setup file utils and search paths
-    [self setupFileUtils];
-    
-    // Setup and load your sprite frames
-    [self setupSpriteFrames];
-    
-    // Setup Cocos2D and run first scene
-    [self setupCocos2d];
+    CCDirectorMac *director = (CCDirectorMac*) [CCDirector sharedDirector];
+
+    // enable FPS and SPF
+    [director setDisplayStats:YES];
+
+    // connect the OpenGL view with the director
+    [director setView:self.glView];
+
+    // 'Effects' don't work correctly when autoscale is turned on.
+    // Use kCCDirectorResize_NoScale if you don't want auto-scaling.
+    //[director setResizeMode:kCCDirectorResize_NoScale];
+
+    // Enable "moving" mouse event. Default no.
+    [self.window setAcceptsMouseMovedEvents:NO];
+
+    // Center main window
+    [self.window center];
+
+    // Configure CCFileUtils to work with SpriteBuilder
+    [CCBReader configureCCFileUtils];
+
+    [director runWithScene:[CCBReader loadAsScene:@"MainScene"]];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification
 {
-}
-
-
-#pragma mark - Helper and setup methods
-
-- (void) setupFileUtils
-{
-    // Configure CCFileUtils to work with SpriteBuilder
-    CCFileUtils *sharedFileUtils = [CCFileUtils sharedFileUtils];
-    
-    sharedFileUtils.directoriesDict = [[NSMutableDictionary alloc] initWithObjectsAndKeys:
-                                       @"resources-phone", CCFileUtilsSuffixMac,
-                                       @"resources-phonehd", CCFileUtilsSuffixMacHD,
-                                       nil];
-    
-    sharedFileUtils.searchPath = [NSArray arrayWithObjects:
-                                  [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"Published-iOS"],
-                                  [[NSBundle mainBundle] resourcePath],
-                                  nil];
-    
-    sharedFileUtils.searchMode = CCFileUtilsSearchModeDirectory;
-    [sharedFileUtils buildSearchResolutionsOrder];
-    [sharedFileUtils loadFilenameLookupDictionaryFromFile:@"fileLookup.plist"];
-}
-
-- (void) setupSpriteFrames
-{
-    [[CCSpriteFrameCache sharedSpriteFrameCache] loadSpriteFrameLookupDictionaryFromFile:@"spriteFrameFileList.plist"];
-}
-
-- (void) setupCocos2d
-{
-    // Setup the CCDirector for Mac
-    CCDirectorMac *director = (CCDirectorMac*) [CCDirector sharedDirector];
-    [director setResizeMode:kCCDirectorResize_NoScale];
-    [director setProjection:CCDirectorProjection2D];
-    
-#ifdef DEBUG
-    // Enable FPS and SPF
-    [director setDisplayStats:YES];
-#endif
-    
-    // Connect the OpenGL view with the director
-    [director setView:self.glView];
-    [self.glView setFrameSize:NSMakeSize(self.window.frame.size.width,self.window.frame.size.height)];
-    
-    // Enable "moving" mouse event. Default no.
-    [self.window setAcceptsMouseMovedEvents:NO];
-    [self.window center];
-    [self.window setRestorable:NO];
-    
-    // Run the first scene
-    [director runWithScene:[self startScene]];
-}
-
-- (CCScene*) startScene
-{
-    return [CCBReader loadAsScene:@"MainScene"];
 }
 
 @end
