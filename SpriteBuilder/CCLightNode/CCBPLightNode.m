258a614a678a7d7407e6668206c43e15e7e84fa6
Thayer J Andrews <thayer@apportable.com>
2014-11-25 13:34:47 -0800

    Light node UI - First pass at light icons in the stage
    They're not in their own layer yet so they scale with other content as the zoom
    level is adjusted. Fixing that is up next.

--- SpriteBuilder/CCLightNode/CCBPLightNode.m
+++ SpriteBuilder/CCLightNode/CCBPLightNode.m
@@ -7,7 +7,34 @@
 //
 
 #import "CCBPLightNode.h"
+#import "CCDirector.h"
+#import "CCDrawNode.h"
+#import "CCSprite.h"
+
+@interface CCBPLightNode ()
+@property (nonatomic, strong) CCNode *lightIcon;
+@end
+
 
 @implementation CCBPLightNode
+
+- (id) init
+{
+    if ((self = [super init]))
+    {
+        _lightIcon = [CCSprite spriteWithImageNamed:@"light-point.png"];
+    }
+
+    return self;
+}
+
+-(void)visit:(CCRenderer *)renderer parentTransform:(const GLKMatrix4 *)parentTransform
+{
+    _lightIcon.position = self.position;
+    _lightIcon.rotation = self.rotation;
+
+    [_lightIcon visit:renderer parentTransform:parentTransform];
+    [super visit:renderer parentTransform:parentTransform];
+}
 
 @end
