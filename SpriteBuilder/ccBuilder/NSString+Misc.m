f5de4e1ebbbb61c057c00ed4fc5aa705b9ff2197
Nicky Weber <nicky.vault13@gmail.com>
2015-02-12 11:22:39 +0100

    BackupFileCommand added for some backup actions during migration. NSString+Misc category with a new method to find a suitable rolling backup filename. Removed some Sparkle artifacts.

--- SpriteBuilder/ccBuilder/NSString+Misc.m
+++ SpriteBuilder/ccBuilder/NSString+Misc.m
@@ -1,4 +1,5 @@
 #import "NSString+Misc.h"
+#import "CCEffect_Private.h"
 
 
 @implementation NSString (Misc)
@@ -8,6 +9,65 @@
     return [self length] == 0
            || ![[self stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] length];
 
+}
+
+- (NSString *)availabeFileNameWithRollingNumberAndExtraExtension:(NSString *)extension
+{
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    if (![fileManager fileExistsAtPath:self])
+    {
+        return nil;
+    }
+
+    NSString *path = [self stringByDeletingLastPathComponent];
+    NSArray *dirContents = [fileManager contentsOfDirectoryAtPath:path error:nil];
+
+    if (!dirContents)
+    {
+        return nil;
+    }
+
+    NSUInteger maxCounter = 0;
+    for (NSString *filename in dirContents)
+    {
+        NSLog(@"- %@", filename);
+        maxCounter = [self highestBackupDirCounterPostfixCurrentCount:maxCounter filename:[path stringByAppendingPathComponent:filename] postfix:extension];
+    }
+
+    NSString *result = extension && ![extension isEmpty]
+        ? [self stringByAppendingPathExtension:extension]
+        : self;
+
+    return [result stringByAppendingPathExtension:[NSString stringWithFormat:@"%lu", maxCounter]];
+}
+
+- (NSUInteger)highestBackupDirCounterPostfixCurrentCount:(NSUInteger)currentCounter filename:(NSString *)directoryName postfix:(NSString *)postfix
+{
+    NSNumber *number = [self parseNumberPostfixInBackupDir:directoryName extension:postfix];
+
+    if (number
+        && ([number unsignedIntegerValue] > currentCounter))
+    {
+        currentCounter = [number unsignedIntegerValue] + 1;
+    }
+
+    return currentCounter;
+}
+
+- (NSNumber *)parseNumberPostfixInBackupDir:(NSString *)directoryName extension:(NSString *)extension
+{
+    NSString *baa2 = [directoryName stringByReplacingOccurrencesOfString:self withString:@""];
+    NSString *baa = [baa2 stringByReplacingOccurrencesOfString:[NSString stringWithFormat:@".%@", extension] withString:@""];
+    NSScanner *scanner = [NSScanner scannerWithString:baa];
+    scanner.charactersToBeSkipped = [NSCharacterSet characterSetWithCharactersInString:@"."];
+
+    NSInteger result;
+    if ([scanner scanInteger:&result])
+    {
+        return @(result);
+    }
+
+    return nil;
 }
 
 @end
