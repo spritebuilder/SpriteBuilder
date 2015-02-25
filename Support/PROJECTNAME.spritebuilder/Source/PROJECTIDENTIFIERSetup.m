2b0792c190d256bc6a1881fc4d9528fd93c8adb4
slembcke <slembcke@gmail.com>
2015-02-25 15:23:47 -0600

    Simplifying the template setup code more.

--- Support/PROJECTNAME.spritebuilder/Source/PROJECTIDENTIFIERSetup.m
+++ Support/PROJECTNAME.spritebuilder/Source/PROJECTIDENTIFIERSetup.m
@@ -9,65 +9,69 @@
 
 @implementation PROJECTIDENTIFIERSetup
 
--(NSDictionary *)baseConfig
+-(NSDictionary *)setupConfig
 {
-    return [super baseConfig];
+    //  Loads configCocos2D.plist from disk or returns an empty dictionary if it doesn't exist.
+    NSMutableDictionary *config = [[super setupConfig] mutableCopy];
+    
+    // Add custom overrides to the cocosConfig.plist file here.
+    [config addEntriesFromDictionary:@{
+//        CCSetupShowDebugStats: @(YES),
+//        CCSetupFixedUpdateInterval: @(1.0/120.0),
+    }];
+    
+    return config;
 }
 
-- (void)setupApplication
+- (void)setupCommon
 {
+    // Cross platform app setup code goes here.
+    
+    // If the project is being used with SpriteBuilder, you'll need some extra search paths, etc.
+    [self setupForSpriteBuilder];
+}
+
+-(CCScene *)createFirstScene
+{
+    // This method is responsible for creating and returning the initial scene when the app starts up.
+    return [CCBReader loadAsScene:@"MainScene"];
+}
+
+#if __CC_PLATFORM_IOS
+
+-(void)setupApplication
+{
+    [self setupCommon];
+    
+    // Your iOS specific setup code goes here.
+    
     [super setupApplication];
 }
 
-- (NSString *)firstSceneName
-{
-    return @"MainScene";
-}
-
-//MARK: iOS
-#if __CC_PLATFORM_IOS
-
--(void)setupIOS
-{
-    [CCFileLocator sharedFileLocator].searchPaths = [@[
-        [[NSBundle mainBundle] pathForResource:@"Published-iOS" ofType:nil],
-    ] arrayByAddingObjectsFromArray:[CCFileLocator sharedFileLocator].searchPaths];
-    
-    [super setupIOS];
-}
-
 #endif
 
-//MARK: Android
 #if __CC_PLATFORM_ANDROID
 
--(void)setupAndroid
+-(void)setupApplication
 {
-    [CCFileLocator sharedFileLocator].searchPaths = [@[
-        [[NSBundle mainBundle] pathForResource:@"Published-Android" ofType:nil],
-    ] arrayByAddingObjectsFromArray:[CCFileLocator sharedFileLocator].searchPaths];
+    [self setupCommon];
     
-    [super setupAndroid];
+    // Your Android specific setup code goes here.
+    
+    [super setupApplication];
 }
 
 #endif
 
-
-//MARK: Mac
 #if __CC_PLATFORM_MAC
 
--(void)setupMac
+-(void)setupApplication
 {
-    [CCFileLocator sharedFileLocator].searchPaths = [@[
-        [[NSBundle mainBundle] pathForResource:@"Published-iOS" ofType:nil],
-    ] arrayByAddingObjectsFromArray:[CCFileLocator sharedFileLocator].searchPaths];
+    [self setupCommon];
     
-    [super setupMac];
-}
-
--(CGSize)defaultWindowSize
-{
-    return CGSizeMake(480.0f, 320.0f);
+    // Your Mac specific setup code goes here.
+    
+    [super setupApplication];
 }
 
 #endif
